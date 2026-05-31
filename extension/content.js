68b120771812c7097fead33e7a397740b7ba3d33
xShive <larsmanneke2300@gmail.com>
2026-06-01 00:16:04 +0200

    Update content.js
    bugfixes in scrapeData()

--- extension/content.js
+++ extension/content.js
@@ -1,18 +1,30 @@
 function scrapeData() {
-    let titleElement = document.querySelector("ep-title");
-    let timestampElements = document.getElementsByClassName("vds-time");
-    let imageElement = document.querySelector("_image_1r2hh_1 _coverImg_2wrhc_89")
+    let titleElement = document.querySelector(".ep-title");
+    let numberElement = document.querySelector(".ep-number")
+    let timestampElements = document.getElementsByClassName("vds-time"); 
+    let imageElement = document.querySelector("img[style*='view-transition-name: poster']")
     let videoElement = document.querySelector("video");
 
-    if (!titleElement || !numberElement || !videoElement) {
+    if (!titleElement || !timestampElements || !imageElement || !videoElement) {
         return null
     }
 
+    let raw_title;
+
+    if (titleElement.textContent.includes("· ")) {
+        let parts = titleElement.textContent.split("· ");
+        raw_title = parts[1].trim();
+    } else {
+        raw_title = titleElement.textContent.trim();
+    }
+    
+
     return {
-        title: titleElement,
-        current: timestampElements[0].getAttribute("data-type"),
-        duration: timestampElements[1].getAttribute("data-type"),
-        cover: imageElement,
+        title: raw_title,
+        episode: numberElement ? numberElement.textContent.trim() : "",
+        current_time: timestampElements[0].textContent,
+        duration: timestampElements[1].textContent,
+        cover: imageElement ? imageElement.src : "",
         paused: videoElement.paused
     };
 }
